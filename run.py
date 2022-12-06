479cd93521d0a5467955a54576fa3250e2ed951c
JoseParra28 <iliftandcook@gmail.com>
2022-12-06 16:01:33 +0000

    create a funtion that delays the response, to make it look like the computer in thinking of a word

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import random
 from words import mainwords
+from time import sleep
+
 username = input("Welcome to HangPerson! \nPlease insert your name\n")
 while len(username) < 1 or username.isalpha() is False:
     print('Please insert letters only \n')
@@ -13,4 +15,10 @@ wrong = []
 def update():
     for i in correct:
         print(i, end='')
-        print()
+        print('Ok!',username, 'let me think of a word')
+
+def loadig():
+    for i in range(5):
+        print('.', end='')
+        sleep(0.4)
+        print()        
