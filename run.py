f47af5ea14e3266e02f15bc81ba7e1fea0629e01
JoseParra28 <iliftandcook@gmail.com>
2022-12-06 18:55:20 +0000

    try a replay funtion

--- run.py
+++ run.py
@@ -3,62 +3,62 @@ from words import mainwords
 from bodyparts import parts
 from time import sleep
 
-username = input("Welcome to HangPerson! \nPlease insert your name\n")
+username = input("Welcome to HangPerson 😎\nPlease insert your name\n")
 while len(username) < 1 or username.isalpha() is False:
-    print('Please insert letters only \n')
-    username = input('Please inser ypu name\n')
+  print('Please insert letters only 💀 \n')
+  username = input("Please insert you name\n")
 
 
 def update():
-    for i in correct:
-        print(i, end='')
-        print('Ok!',username, 'let me think of a word')
+   for i in correct:
+    print(i, end=' ')
+   print()
+print('Let me think of a word', username)
 
-def loadig():
-    for i in range(4):
-        print('.', end='')
-        sleep(0.4)
-        print()  
+def loading():
+  for i in range(5):
+    print('.', end = '' )
+    sleep(0.4)
+  print() 
 
-    loadig()              
+loading() 
+
 
 picked = random.choice(mainwords)
 print('The word has', len(picked), 'letters.')
 correct = ['_'] * len(picked)
 wrong = []
 
-update()
-parts(len(wrong))
+update() 
+parts(len(wrong)) 
 
 while True:
 
-    guess = input('Please guess a letter\n')
-    print('Let me check...')
-    loadig()
+  guess = input("Please guess a letter\n")
+  print('Let me check...')
+  loading()
 
-    if guess in picked:
-        index = 0
-        for i in picked:
-            if i == guess:
-                correct[index] = guess
-                print("That's correct", username)
-                index += 1 
-                update()
-                parts(len(wrong))
-
-            else:
-                if guess not in wrong:
-                    wrong.append(guess)
-                    parts(len(wrong))
-                    print("That's not correct")
-                else:
-                    print('You already guessed that!')
-                if len(wrong) > 4:
-                    print('Oh no' , username , "you didn't win this time\n Game over !!")
-                    print('I picked', [picked])
-                    break 
-                if '_' not in correct:
-                    print('Yay' , username , 'you won!')
-                    break 
-
-
+  if guess in picked:
+    index = 0 
+    for i in picked:
+      if i == guess:
+        correct[index] = guess
+        print("That's correct! 🤟")
+      index += 1
+    update()  
+    parts(len(wrong))
+   
+  else:
+      if guess not in wrong:
+          wrong.append(guess)
+          parts(len(wrong))
+          print("That's not correct 🥺, try another letter.\n You wrote:", wrong)
+      else:
+          print('You already guessed that!🤯') 
+  if len(wrong) > 4:
+    print("Oh no",username,"you didn't win this time 💀\n Game over! 🎮")
+    print(' I picked', {picked})
+    break      
+  if '_' not in correct:
+       print('Yay!',username,'you won!!! 🙌') 
+       break
