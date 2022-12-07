cffeda1b728c6e7259f55baef332d1bf33551f14
JoseParra28 <iliftandcook@gmail.com>
2022-12-07 15:48:44 +0000

    add messages to indicate what each peace of code does

--- run.py
+++ run.py
@@ -5,6 +5,9 @@ from time import sleep
 
 
 class Welcome:
+  """
+  Main class, prints out a welcome message as well as the game rules
+  """
   def __init__(self, message,rules):
     self.message = message
     self.rules = rules
@@ -14,19 +17,26 @@ pregame = Welcome('hello', 'gamerules')
 print('\nWelcome to HangPerson\n')
 print('Game rules: I will randomly pick a word\n and you will have to guess each letter of the word,\n if the letter is correct, it will be added to the correct letters,\n but if is not correct, you will be slowly hung\n Good Luck!!')
 
-
+"""
+ Username is validated, it can only contain at least on letter, no numbers.
+"""
 username = input("Please insert your name\n")
 while len(username) < 1 or username.isalpha() is False:
   print('Please insert letters only 💀 \n')
   username = input("Please insert you name\n")
 
-
+""" 
+Main funtion, it checks for correct or wrong letters
+"""
 def update():
    for i in correct:
     print(i, end=' ')
    print()
 print('Let me think of a word', username)
 
+"""
+Time delay funtion, simulates interaction with the user, making it look like the game in thinking.
+"""
 def loading():
   for i in range(6):
     print('-', end = '' )
@@ -35,7 +45,11 @@ def loading():
 
 loading() 
 
-
+"""
+this variable picks a random word from the external list.
+There are also 2 lists one for correct and one fro wrong
+the correct list multiplies and amount of letters in the selected word
+"""
 picked = random.choice(mainwords)
 print('Ok',username,'the word has', len(picked), 'letters.')
 correct = ['_'] * len(picked)
@@ -43,7 +57,10 @@ wrong = []
 
 update() 
 parts(len(wrong)) 
-
+"""
+ Main game loop, it asks for an input, it also validates if the user types the wrong letter twice
+ if the user looses, it will display the picked word at the end of the loop
+"""
 while True:
 
   guess = input("Please guess a letter\n")
